c420a4af339297d5bfd554ab8766b24241046d76
pyaephyohein <pyaephyohein.info.3326@gmail.com>
2020-04-22 12:11:56 +0630

    unicode to zawgyi and zawgyi to unicode

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import myanmar.encodings
 import myanmar.converter
+from flask import Flask,render_template,request
 
-def main():
-    text = input("Unicode Here >>>  ")
-    conv = myanmar.converter.convert(text,'unicode','zawgyi')
-    print(f'Zawgyi is >>>{conv}')
+app = Flask(__name__)
+@app.route('/zawgyitounicode',methods = ['GET','POST'])
+def zawgyitouni():
+    if request.method == 'GET':
+        return render_template("index.html")
+    else:
+        input_mm = request.form['input_mm']
+        conv = myanmar.converter.convert(input_mm,'zawgyi','unicode')
+        return render_template('index.html',conv=conv)
+@app.route('/unicodetozawgyi',methods = ['GET','POST'])
+def unitozawgyi():
+    if request.method == 'GET':
+        return render_template("index.html")
+    else:
+        input_mm = request.form['input_mm']
+        conv = myanmar.converter.convert(input_mm,'unicode','zawgyi')
+        return render_template('index.html',conv=conv)
 
 if __name__=="__main__":
-    main()
+    app.run(host="0.0.0.0",port=8888,debug=True)
 
